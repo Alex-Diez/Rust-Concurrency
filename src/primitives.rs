3b0327a62ec9f9361d09c32336e59b413b5e0ad1
Alex-Diez <alexvic.amatory@gmail.com>
2016-01-03 15:38:12 +0200

    semaphore constructor

---
+++ src/primitives.rs
@@ -0,0 +1,8 @@
+pub struct Semaphore;
+
+impl Semaphore {
+    
+    pub fn new(permitions: usize) -> Semaphore {
+        Semaphore
+    }
+}
--- test/test_primitives.rs
+++ test/test_primitives.rs
@@ -1,4 +1,12 @@
-#[test]
-fn nothing() {
-	
+extern crate concurrent;
+
+#[cfg(test)]
+mod semaphore_prim {
+
+    use super::concurrent::primitives::Semaphore;
+
+    #[test]
+    fn it_should_create_semaphore() {
+        Semaphore::new(1);
+    }
 }
