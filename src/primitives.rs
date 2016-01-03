676aa41eccd47d3c5c56d002d2c666a333d8816d
Alex-Diez <alexvic.amatory@gmail.com>
2016-01-03 15:49:32 +0200

    simple try acquire case

--- src/primitives.rs
+++ src/primitives.rs
@@ -1,8 +1,20 @@
-pub struct Semaphore;
+use std::option::Option;
+
+pub struct Semaphore {
+    permitions: usize
+}
 
 impl Semaphore {
     
     pub fn new(permitions: usize) -> Semaphore {
-        Semaphore
+        Semaphore { permitions: permitions }
+    }
+
+    pub fn acquire(&mut self) -> Option<usize> {
+        Some(self.permitions)
+    }
+
+    pub fn try_acquire(&mut self) -> Option<usize> {
+        None
     }
 }
--- test/test_primitives.rs
+++ test/test_primitives.rs
@@ -5,8 +5,24 @@ mod semaphore_prim {
 
     use super::concurrent::primitives::Semaphore;
 
+    use std::thread;
+
     #[test]
     fn it_should_create_semaphore() {
         Semaphore::new(1);
+    }
+
+    #[test]
+    fn thread_should_try_to_acquire_resource() {
+        let mut s = Semaphore::new(1);
+
+        let res = s.acquire();
+        thread::spawn(
+            move || {
+                let try_res = s.try_acquire();
+                assert_eq!(try_res, None);
+            }
+        );
+        assert_eq!(res, Some(1));
     }
 }
