07617c08f1be8e2dbf592ad2718776ff4e5bd597
Alex-Diez <alexvic.amatory@gmail.com>
2016-01-06 13:37:40 +0200

    rename to Resource Holder semaphore

--- src/primitives.rs
+++ src/primitives.rs
@@ -1,22 +1,21 @@
 use std::option::Option;
 use std::collections::HashMap;
 
-pub struct Semaphore {
+pub struct ResourceHolderSemaphore {
     resources: HashMap<usize, usize>,
     current_permissions: usize,
 }
 
-impl Semaphore {
-    pub fn new(permissions: usize) -> Semaphore {
-        let mut resources = HashMap::with_capacity(permissions);
-        for i in (1..permissions + 1) {
-            resources.insert(i, 0);
+impl ResourceHolderSemaphore {
+    pub fn new(permissions: usize) -> ResourceHolderSemaphore {
+        ResourceHolderSemaphore {
+            resources: HashMap::with_capacity(permissions),
+            current_permissions: permissions
         }
-        Semaphore { resources: resources, current_permissions: permissions }
     }
 
-    pub fn acquire(&mut self) -> Option<usize> {
-        self.acuqire_resource()
+    pub fn acquire(&mut self) -> usize {
+        self.acuqire_resource().unwrap_or(0)
     }
 
     pub fn try_acquire(&mut self) -> Option<usize> {
@@ -28,12 +27,16 @@ impl Semaphore {
     }
 
     pub fn update(&mut self, index: usize, val: usize) {
-       let res_val = self.resources.get_mut(&index);
-       *(res_val.unwrap()) = val;
+        if self.resources.contains_key(&index) {
+            let res_val = self.resources.get_mut(&index);
+            *(res_val.unwrap()) = val;
+        } else {
+            self.resources.insert(index, val);
+        }
     }
 
-    pub fn get(&self, index: usize) -> Option<usize> {
-        self.resources.get(&index).map(|v| *v)
+    pub fn get(&self, index: usize) -> usize {
+        self.resources.get(&index).map(|v| *v).unwrap_or(0)
     }
 
     fn acuqire_resource(&mut self) -> Option<usize> {
--- test/test_primitives.rs
+++ test/test_primitives.rs
@@ -3,13 +3,13 @@ extern crate concurrent;
 #[cfg(test)]
 mod semaphore_prim {
 
-    use super::concurrent::primitives::Semaphore;
+    use super::concurrent::primitives::ResourceHolderSemaphore;
 
     use std::thread;
 
     #[test]
     fn it_should_not_acquired_more_than_semaphore_contains_with_try_acquire() {
-        let mut s = Semaphore::new(3);
+        let mut s = ResourceHolderSemaphore::new(3);
 
         let res1 = s.try_acquire();
         let res2 = s.try_acquire();
@@ -24,7 +24,7 @@ mod semaphore_prim {
 
     #[test]
     fn thread_should_try_to_acquire_resource() {
-        let mut s = Semaphore::new(1);
+        let mut s = ResourceHolderSemaphore::new(1);
 
         let res = s.acquire();
         thread::spawn(
@@ -33,80 +33,79 @@ mod semaphore_prim {
                 assert_eq!(try_res, None);
             }
         );
-        assert_eq!(res, Some(1));
+        assert_eq!(res, 1);
     }
 
     #[test]
     fn it_should_acquire_each_time_new_resource() {
-        let mut s = Semaphore::new(3);
+        let mut s = ResourceHolderSemaphore::new(3);
 
         let res3 = s.acquire();
         let res2 = s.acquire();
 
-        assert_eq!(res3, Some(3));
-        assert_eq!(res2, Some(2));
+        assert_eq!(res3, 3);
+        assert_eq!(res2, 2);
     }
 
     #[test]
     fn acquire_released_resource() {
-        let mut s = Semaphore::new(3);
+        let mut s = ResourceHolderSemaphore::new(3);
 
         let res = s.acquire();
-        assert_eq!(res, Some(3));
+        assert_eq!(res, 3);
         s.release();
 
         let reacquired_res = s.acquire();
-        assert_eq!(reacquired_res, Some(3));
+        assert_eq!(reacquired_res, 3);
     }
 
     #[test]
     fn acquire_update_release_acquire_resource_with_new_value() {
-        let mut s = Semaphore::new(1);
+        let mut s = ResourceHolderSemaphore::new(1);
         let res_index = s.acquire();
 
-        update_and_released(&mut s, 10, res_index.unwrap());
+        update_and_released(&mut s, 10, res_index);
 
         let res_index = s.acquire();
-        let res_val = s.get(res_index.unwrap());
+        let res_val = s.get(res_index);
 
-        assert_eq!(res_val, Some(10));
+        assert_eq!(res_val, 10);
     }
 
-    fn update_and_released(semaphore: &mut Semaphore, update_to: usize, index: usize) {
+    fn update_and_released(semaphore: &mut ResourceHolderSemaphore, update_to: usize, index: usize) {
         semaphore.update(index, update_to);
         semaphore.release();
     }
 
     #[test]
     fn acquire_update_released_multiple_resources() {
-        let mut s = Semaphore::new(3);
+        let mut s = ResourceHolderSemaphore::new(3);
         let res_index3 = s.acquire();
         let res_index2 = s.acquire();
         let res_index1 = s.acquire();
 
-        update_and_released(&mut s, 10, res_index3.unwrap());
-        update_and_released(&mut s, 20, res_index2.unwrap());
-        update_and_released(&mut s, 30, res_index1.unwrap());
+        update_and_released(&mut s, 10, res_index3);
+        update_and_released(&mut s, 20, res_index2);
+        update_and_released(&mut s, 30, res_index1);
 
         let res_index3 = s.acquire();
         let res_index2 = s.acquire();
         let res_index1 = s.acquire();
 
-        assert_eq!(s.get(res_index3.unwrap()), Some(10));
-        assert_eq!(s.get(res_index2.unwrap()), Some(20));
-        assert_eq!(s.get(res_index1.unwrap()), Some(30));
+        assert_eq!(s.get(res_index3), 10);
+        assert_eq!(s.get(res_index2), 20);
+        assert_eq!(s.get(res_index1), 30);
     }
 
     #[test]
     fn it_should_not_modify_semaphore_resource_when_modify_local_value_only() {
-        let mut s = Semaphore::new(1);
+        let mut s = ResourceHolderSemaphore::new(1);
         let index = s.acquire();
 
-        let res = s.get(index.unwrap());
-        let res = res.map(|v| v + 10);
+        let res = s.get(index) + 10;
 
-        assert_eq!(res, Some(10));
-        assert_eq!(s.get(index.unwrap()), Some(0));
+        assert_eq!(res, 10);
+        assert_eq!(s.get(index), 0);
     }
 
     //todo
