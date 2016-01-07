50f1f8a75c29bc78f77df7bef8775723767280f7
Alex-Diez <alexvic.amatory@gmail.com>
2016-01-07 11:56:42 +0200

    test count down latch with multiple threads

--- src/primitives.rs
+++ src/primitives.rs
@@ -1,11 +1,4 @@
-use std::option::Option;
-use std::collections::HashMap;
 use std::sync::{Condvar, Mutex};
-
-pub struct CountDownLatch {
-    sync: Mutex<LatchStatus>,
-    condition: Condvar
-}
 
 struct LatchStatus {
     counts: usize
@@ -16,6 +9,11 @@ impl LatchStatus {
     pub fn new(counts: usize) -> LatchStatus {
         LatchStatus { counts: counts }
     }
+}
+
+pub struct CountDownLatch {
+    sync: Mutex<LatchStatus>,
+    condition: Condvar
 }
 
 impl CountDownLatch {
@@ -41,54 +39,5 @@ impl CountDownLatch {
 
     pub fn get_counts(&self) -> usize {
         self.sync.lock().unwrap().counts
-    }
-}
-
-pub struct ResourceHolderSemaphore {
-    resources: HashMap<usize, usize>,
-    current_permissions: usize,
-}
-
-impl ResourceHolderSemaphore {
-    pub fn new(permissions: usize) -> ResourceHolderSemaphore {
-        ResourceHolderSemaphore {
-            resources: HashMap::with_capacity(permissions),
-            current_permissions: permissions
-        }
-    }
-
-    pub fn acquire(&mut self) -> usize {
-        self.acuqire_resource().unwrap_or(0)
-    }
-
-    pub fn try_acquire(&mut self) -> Option<usize> {
-        self.acuqire_resource()
-    }
-
-    pub fn release(&mut self) {
-        self.current_permissions += 1;
-    }
-
-    pub fn update(&mut self, index: usize, val: usize) {
-        if self.resources.contains_key(&index) {
-            let res_val = self.resources.get_mut(&index);
-            *(res_val.unwrap()) = val;
-        } else {
-            self.resources.insert(index, val);
-        }
-    }
-
-    pub fn get(&self, index: usize) -> usize {
-        self.resources.get(&index).map(|v| *v).unwrap_or(0)
-    }
-
-    fn acuqire_resource(&mut self) -> Option<usize> {
-        if self.current_permissions > 0 {
-            let res = self.current_permissions;
-            self.current_permissions -= 1;
-            Some(res)
-        } else {
-            None
-        }   
     }
 }
--- test/test_primitives.rs
+++ test/test_primitives.rs
@@ -20,140 +20,32 @@ mod count_down_latch_prim {
 
     #[test]
     fn thread_should_await_until_counts_not_equals_zero() {
+        const NUMBER_OF_THREADS: usize = 10;
         let arc = Arc::new(CountDownLatch::new(1));
+        let mut results = Vec::with_capacity(NUMBER_OF_THREADS);
 
-        let count_down_latch = arc.clone();
-        let t = thread::spawn(
-            move || {
-                assert_eq!(count_down_latch.get_counts(), 1);
+        for _ in 0..NUMBER_OF_THREADS {
+            let count_down_latch = arc.clone();
+            let jh = thread::spawn(
+                move || {
+                    assert!(count_down_latch.get_counts() > 0);
 
-                count_down_latch.await();
+                    count_down_latch.await();
 
-                assert_eq!(count_down_latch.get_counts(), 0);
-            }
-        );
+                    assert!(count_down_latch.get_counts() == 0);
+                }
+            );
+            results.push(jh);
+        }
 
         thread::sleep(Duration::from_millis(100));
 
         arc.count_down();
 
-        let res = t.join();
-        assert!(res.is_ok());
+        assert_eq!(arc.get_counts(), 0);
+        for jh in results {
+            let res = jh.join();
+            assert!(res.is_ok());
+        }
     }
-}
-
-#[cfg(test)]
-mod semaphore_prim {
-
-    use super::concurrent::primitives::ResourceHolderSemaphore;
-
-    use std::thread;
-
-    #[test]
-    fn it_should_not_acquired_more_than_semaphore_contains_with_try_acquire() {
-        let mut s = ResourceHolderSemaphore::new(3);
-
-        let res1 = s.try_acquire();
-        let res2 = s.try_acquire();
-        let res3 = s.try_acquire();
-        let res4 = s.try_acquire();
-
-        assert_eq!(res1, Some(3));
-        assert_eq!(res2, Some(2));
-        assert_eq!(res3, Some(1));
-        assert_eq!(res4, None);
-    }
-
-    #[test]
-    fn thread_should_try_to_acquire_resource() {
-        let mut s = ResourceHolderSemaphore::new(1);
-
-        let res = s.acquire();
-        thread::spawn(
-            move || {
-                let try_res = s.try_acquire();
-                assert_eq!(try_res, None);
-            }
-        );
-        assert_eq!(res, 1);
-    }
-
-    #[test]
-    fn it_should_acquire_each_time_new_resource() {
-        let mut s = ResourceHolderSemaphore::new(3);
-
-        let res3 = s.acquire();
-        let res2 = s.acquire();
-
-        assert_eq!(res3, 3);
-        assert_eq!(res2, 2);
-    }
-
-    #[test]
-    fn acquire_released_resource() {
-        let mut s = ResourceHolderSemaphore::new(3);
-
-        let res = s.acquire();
-        assert_eq!(res, 3);
-        s.release();
-
-        let reacquired_res = s.acquire();
-        assert_eq!(reacquired_res, 3);
-    }
-
-    #[test]
-    fn acquire_update_release_acquire_resource_with_new_value() {
-        let mut s = ResourceHolderSemaphore::new(1);
-        let res_index = s.acquire();
-
-        update_and_released(&mut s, 10, res_index);
-
-        let res_index = s.acquire();
-        let res_val = s.get(res_index);
-
-        assert_eq!(res_val, 10);
-    }
-
-    fn update_and_released(semaphore: &mut ResourceHolderSemaphore, update_to: usize, index: usize) {
-        semaphore.update(index, update_to);
-        semaphore.release();
-    }
-
-    #[test]
-    fn acquire_update_released_multiple_resources() {
-        let mut s = ResourceHolderSemaphore::new(3);
-        let res_index3 = s.acquire();
-        let res_index2 = s.acquire();
-        let res_index1 = s.acquire();
-
-        update_and_released(&mut s, 10, res_index3);
-        update_and_released(&mut s, 20, res_index2);
-        update_and_released(&mut s, 30, res_index1);
-
-        let res_index3 = s.acquire();
-        let res_index2 = s.acquire();
-        let res_index1 = s.acquire();
-
-        assert_eq!(s.get(res_index3), 10);
-        assert_eq!(s.get(res_index2), 20);
-        assert_eq!(s.get(res_index1), 30);
-    }
-
-    #[test]
-    fn it_should_not_modify_semaphore_resource_when_modify_local_value_only() {
-        let mut s = ResourceHolderSemaphore::new(1);
-        let index = s.acquire();
-
-        let res = s.get(index) + 10;
-
-        assert_eq!(res, 10);
-        assert_eq!(s.get(index), 0);
-    }
-
-    //todo
-    //1. test when two thread modify different resources
-    //2. find a way how to prevent use resource when thread release it
-    //3. test for locking thread on acquire method if there is no free resources
-    // optinal / specific functionality
-    //4. acquire resourse which was released previously by the thread
 }
