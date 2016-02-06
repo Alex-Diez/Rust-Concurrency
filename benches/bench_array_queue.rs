3679bed3f1115b7b473d01836fc52d786703d224
Alex-Diez <alexvic.amatory@gmail.com>
2016-02-06 11:30:55 +0200

    MCMP SCMP benchmarks

--- benches/bench_array_queue.rs
+++ benches/bench_array_queue.rs
@@ -8,79 +8,195 @@ extern crate expectest;
 pub use concrust::queue::ArrayBlockingQueue;
 pub use concrust::queue::BlockingQueue;
 
-pub use expectest::prelude::be_ok;
+pub use expectest::prelude::{be_equal_to, be_ok};
 
 pub use std::thread;
 pub use std::thread::JoinHandle;
 pub use std::sync::{Arc, Barrier};
+pub use std::result::Result;
 
 const NUMBER_OF_ELEMENTS: i64 = 1000;
+const NUMBER_OF_PROCCESS: i64 = 5;
 
 #[bench]
-fn consumer_producer_default_queue_size(bencher: &mut test::Bencher) {
-    // queue size will be 16
-    let queue: ArrayBlockingQueue<i64> = ArrayBlockingQueue::new();
-    let barrier = Arc::new(Barrier::new(3));
-    let result = sum(NUMBER_OF_ELEMENTS);
+fn multiple_consumers_multiple_producer_default_queue_size(bencher: &mut test::Bencher) {
+    let (operation_number, expected_result) = multiple_consumers_producer_setup();
     bencher.iter(
         || {
-            let c_handle = spawn_consumer(&queue, &barrier);
-            let p_handle = spawn_producer(&queue, &barrier);
-            barrier.wait();
-            expect!(c_handle.join()).to(be_ok().value(result));
-            p_handle.join()
+            let actual_result = multiple_consumers_multiple_producer_iter(
+                ArrayBlockingQueue::new(),
+                Arc::new(Barrier::new(11)),
+                operation_number
+            );
+            expect!(actual_result).to(be_equal_to(expected_result));
         }
     );
 }
 
 #[bench]
-fn consumer_producer_small_queue_size(bencher: &mut test::Bencher) {
-            let queue_size = (NUMBER_OF_ELEMENTS / 20) as usize;
-            // queue size will be 1000 / 20 >> 64
-            let queue: ArrayBlockingQueue<i64> = ArrayBlockingQueue::with_capacity(queue_size);
-            let barrier = Arc::new(Barrier::new(3));
-            let result = sum(NUMBER_OF_ELEMENTS);
-            bencher.iter(
-                || {
-                    let c_handle = spawn_consumer(&queue, &barrier);
-                    let p_handle = spawn_producer(&queue, &barrier);
-                    barrier.wait();
-                    expect!(c_handle.join()).to(be_ok().value(result));
-                    p_handle.join()
-                }
-            );
-        }
-
-#[bench]
-fn consumer_producer_large_queue_size(bencher: &mut test::Bencher) {
-    let queue_size = (NUMBER_OF_ELEMENTS / 2) as usize;
-    // queue size will be 1000 / 2 >> 512
-    let queue: ArrayBlockingQueue<i64> = ArrayBlockingQueue::with_capacity(queue_size);
-    let barrier = Arc::new(Barrier::new(3));
-    let result = sum(NUMBER_OF_ELEMENTS);
+fn multiple_consumers_multiple_producer_small_queue_size(bencher: &mut test::Bencher) {
+    let (operation_number, expected_result) = multiple_consumers_producer_setup();
     bencher.iter(
         || {
-            let c_handle = spawn_consumer(&queue, &barrier);
-            let p_handle = spawn_producer(&queue, &barrier);
-            barrier.wait();
-            expect!(c_handle.join()).to(be_ok().value(result));
-            p_handle.join()
+            let actual_result = multiple_consumers_multiple_producer_iter(
+                ArrayBlockingQueue::with_capacity((NUMBER_OF_ELEMENTS / 20) as usize),
+                Arc::new(Barrier::new(11)),
+                operation_number
+            );
+            expect!(actual_result).to(be_equal_to(expected_result));
         }
     );
+}
+
+#[bench]
+fn multiple_consumers_multiple_producer_large_queue_size(bencher: &mut test::Bencher) {
+    let (operation_number, expected_result) = multiple_consumers_producer_setup();
+    bencher.iter(
+        || {
+            let actual_result = multiple_consumers_multiple_producer_iter(
+                ArrayBlockingQueue::with_capacity((NUMBER_OF_ELEMENTS / 2) as usize),
+                Arc::new(Barrier::new(11)),
+                operation_number
+            );
+            expect!(actual_result).to(be_equal_to(expected_result));
+        }
+    );
+}
+
+fn multiple_consumers_multiple_producer_iter(queue: ArrayBlockingQueue<i64>, barrier: Arc<Barrier>, oper: i64) -> i64 {
+    let mut consumers = Vec::with_capacity(NUMBER_OF_PROCCESS as usize);
+    for _ in 0..NUMBER_OF_PROCCESS {
+        consumers.push(spawn_consumer(&queue, &barrier, oper));
+        spawn_producer(&queue, &barrier, oper);
+    }
+    barrier.wait();
+    consumers.into_iter().map(|c| c.join().unwrap()).fold(0, |acc, v| { acc + v } )
+}
+
+fn multiple_consumers_producer_setup() -> (i64, i64) {
+    let operation_number = NUMBER_OF_ELEMENTS / NUMBER_OF_PROCCESS;
+    (operation_number, sum(operation_number) * NUMBER_OF_PROCCESS)
+}
+
+#[bench]
+fn single_consumers_multiple_producer_default_queue_size(bencher: &mut test::Bencher) {
+    let (operation_number, expected_result) = multiple_consumers_producer_setup();
+    bencher.iter(
+        || {
+            let actual_result = single_consumers_multiple_producer_iter(
+                ArrayBlockingQueue::new(),
+                Arc::new(Barrier::new(7)),
+                operation_number
+            );
+            expect!(actual_result).to(be_equal_to(expected_result))
+        }
+    );
+}
+
+#[bench]
+fn single_consumers_multiple_producer_small_queue_size(bencher: &mut test::Bencher) {
+    let (operation_number, expected_result) = multiple_consumers_producer_setup();
+    bencher.iter(
+        || {
+            let actual_result = single_consumers_multiple_producer_iter(
+                ArrayBlockingQueue::with_capacity((NUMBER_OF_PROCCESS / 20) as usize),
+                Arc::new(Barrier::new(7)),
+                operation_number
+            );
+            expect!(actual_result).to(be_equal_to(expected_result))
+        }
+    );
+}
+
+#[bench]
+fn single_consumers_multiple_producer_large_queue_size(bencher: &mut test::Bencher) {
+    let (oper, expected_result) = multiple_consumers_producer_setup();
+    bencher.iter(
+        || {
+            let actual_result = single_consumers_multiple_producer_iter(
+                ArrayBlockingQueue::with_capacity((NUMBER_OF_PROCCESS / 2) as usize),
+                Arc::new(Barrier::new(7)),
+                oper
+            );
+            expect!(actual_result).to(be_equal_to(expected_result))
+        }
+    );
+}
+
+fn single_consumers_multiple_producer_iter(queue: ArrayBlockingQueue<i64>, barrier: Arc<Barrier>, oper: i64) -> i64{
+    let c_handle = spawn_consumer(&queue, &barrier, NUMBER_OF_PROCCESS);
+    for _ in 0..NUMBER_OF_PROCCESS {
+        spawn_producer(&queue, &barrier, oper);
+    }
+    barrier.wait();
+    c_handle.join().unwrap()
+}
+
+#[bench]
+fn single_consumer_single_producer_default_queue_size(bencher: &mut test::Bencher) {
+    let expected_result = single_consumer_single_producer_setup();
+    bencher.iter(
+        || {
+            let actual_result = single_consumer_single_producer_iter(
+                ArrayBlockingQueue::new(),
+                Arc::new(Barrier::new(3))
+            );
+            expect!(actual_result).to(be_equal_to(expected_result));
+        }
+    );
+}
+
+#[bench]
+fn single_consumer_single_producer_small_queue_size(bencher: &mut test::Bencher) {
+    let expected_result = single_consumer_single_producer_setup();
+    bencher.iter(
+        || {
+            let actual_result = single_consumer_single_producer_iter(
+                ArrayBlockingQueue::with_capacity((NUMBER_OF_ELEMENTS / 20) as usize),
+                Arc::new(Barrier::new(3))
+            );
+            expect!(actual_result).to(be_equal_to(expected_result));
+        }
+    );
+}
+
+#[bench]
+fn single_consumer_single_producer_large_queue_size(bencher: &mut test::Bencher) {
+    let expected_result = single_consumer_single_producer_setup();
+    bencher.iter(
+        || {
+            let actual_result = single_consumer_single_producer_iter(
+                ArrayBlockingQueue::with_capacity((NUMBER_OF_ELEMENTS / 2) as usize),
+                Arc::new(Barrier::new(3))
+            );
+            expect!(actual_result).to(be_equal_to(expected_result));
+        }
+    );
+}
+
+fn single_consumer_single_producer_iter(queue: ArrayBlockingQueue<i64>, barrier: Arc<Barrier>) -> i64 {
+    let handle = spawn_consumer(&queue, &barrier, NUMBER_OF_ELEMENTS);
+    spawn_producer(&queue, &barrier, NUMBER_OF_ELEMENTS);
+    barrier.wait();
+    handle.join().unwrap()
+}
+
+fn single_consumer_single_producer_setup() -> i64 {
+    sum(NUMBER_OF_ELEMENTS)
 }
 
 pub fn sum(last: i64) -> i64 {
     (last - 1) * last / 2
 }
 
-pub fn spawn_consumer<'q, 'b>(queue: &'q ArrayBlockingQueue<i64>, barrier: &'b Arc<Barrier>) -> JoinHandle<i64> {
+pub fn spawn_consumer<'q, 'b>(queue: &'q ArrayBlockingQueue<i64>, barrier: &'b Arc<Barrier>, oper: i64) -> JoinHandle<i64> {
     let consume = queue.clone();
     let barrier_cons = barrier.clone();
     thread::spawn(
         move || {
             barrier_cons.wait();
             let mut sum = 0;
-            for _ in 0..NUMBER_OF_ELEMENTS {
+            for _ in 0..oper {
                 sum += consume.dequeue();
             }
             sum
@@ -88,15 +204,15 @@ pub fn spawn_consumer<'q, 'b>(queue: &'q ArrayBlockingQueue<i64>, barrier: &'b A
     )
 }
 
-pub fn spawn_producer<'q, 'b>(queue: &'q ArrayBlockingQueue<i64>, barrier: &'b Arc<Barrier>) -> JoinHandle<()> {
+pub fn spawn_producer<'q, 'b>(queue: &'q ArrayBlockingQueue<i64>, barrier: &'b Arc<Barrier>, oper: i64) {
     let produce = queue.clone();
     let barrier_prod = barrier.clone();
     thread::spawn(
         move || {
             barrier_prod.wait();
-            for i in 0..NUMBER_OF_ELEMENTS {
+            for i in 0..oper {
                 produce.enqueue(i);
             }
         }
-    )
+    );
 }
